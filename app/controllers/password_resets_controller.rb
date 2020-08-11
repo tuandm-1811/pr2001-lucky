c08969554eb4a55b87acb5467f12e305f197dc55
Clausyy <dominhtuan18112@gmail.com>
2020-08-11 21:25:02 +0700

    old school

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,9 +4,9 @@ class PasswordResetsController < ApplicationController
     def create
         @user = User.find_by(email: params[:password_reset][:email].downcase)
             if @user
-            @user.create_reset_digest
-            @user.send_password_reset_email
-            flash[:info] = "Email sent with password reset instructions"
+            @user.created_at_before_last_save
+            # tra ve gia tri goc trc khi save
+            flash[:success] = "Email sent with password reset instructions"
             redirect_to root_url
         else
             flash.now[:danger] = "Email address not found"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       log_in @user
       flash[:success] = 'User created'
       redirect_to @user
